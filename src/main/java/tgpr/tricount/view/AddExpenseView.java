2febf8a996eda6c3aaa176cffa98f964cc248085
Yannick Denis <1509yadenis@student.epfc.eu>
2023-10-13 10:12:43 +0200

    feat(add_operation):suite du panel +coment

--- src/main/java/tgpr/tricount/view/AddExpenseView.java
+++ src/main/java/tgpr/tricount/view/AddExpenseView.java
@@ -4,13 +4,21 @@ import com.googlecode.lanterna.gui2.*;
 import tgpr.framework.Layouts;
 import tgpr.tricount.controller.AddExpenseController;
 import tgpr.tricount.controller.TestController;
+import tgpr.tricount.model.Operation;
+import tgpr.tricount.model.Tricount;
+import tgpr.tricount.model.User;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
 
+
+
 public class AddExpenseView extends BasicWindow {
     private AddExpenseController controler;
-
+    private  Operation operation;
 
     public AddExpenseView(AddExpenseController controler) {
         this.controler = controler;
@@ -20,28 +28,33 @@ public class AddExpenseView extends BasicWindow {
 
         Panel root = new Panel();
         setComponent(root);
-
+        //creation du panel
         Panel panel = new Panel().setLayoutManager(new GridLayout(2).setTopMarginSize(1).setVerticalSpacing(1))
                 .setLayoutData(Layouts.LINEAR_CENTER).addTo(root);
+        //ajout de label au panel et de leur box pour recuillir les donne
         panel.addComponent(new Label("Titlte:"));
         TextBox txtTitle = new TextBox().addTo(panel);
         panel.addComponent(new Label("Amount:"));
         TextBox txtAmount = new TextBox().addTo(panel);
         panel.addComponent(new Label("Date:"));
         TextBox Date = new TextBox().addTo(panel)
+                //validation que c'est une date
                 .setValidationPattern(Pattern.compile("[/\\d]{0,10}"));
         panel.addComponent(new Label("Pay By:"));
-        ComboBox<String> cklRole=new ComboBox<>();
-        cklRole.addItem("cc");
-        panel.addComponent(cklRole);
+        ComboBox<String> payBy=new ComboBox<>();
+        payBy.addItem("cc");
+        panel.addComponent(payBy);
         panel.addComponent(new Label("use a repartition \n template (optional) "));
+        //ajout de bouton
         var btnAply = new Button("Apply", () -> {
-            System.out.println("apply");
+            //logique du bouton
         }).addTo(root);
         ComboBox<String> selectTampletate=new ComboBox<>();
         selectTampletate.addItem("No ,I use a custuom repartition ");
         panel.addComponent(selectTampletate);
         panel.addComponent(new Label("for Whom :\n (wheight <-/-> or -/+)"));
+
+        new EmptySpace().addTo(root);
         var btnSave = new Button("Save", () -> {
 
         }).addTo(root);
@@ -51,6 +64,10 @@ public class AddExpenseView extends BasicWindow {
         var btnCancel = new Button("Cancel", () -> {
 
         }).addTo(root);
+        //cration de l'operation avec les donne recuilli pour pouvoir l'ajoute en DB apres
+        operation =new Operation(txtTitle, ,txtAmount,new Date(String.valueOf(Date)),
+                                  User.getByFullName(payBy.getSelectedItem()).getId(),
+                LocalDateTime.now());
 
 
     }
