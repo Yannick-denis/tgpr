f4afd95027c5059a2ddfac11427d2413043bb930
Lucas Flores <lucas.flores.ruiz@live.be>
2023-10-19 16:11:02 +0200

    feat(add_tricount):add_tricountView textbox pour titre et description

--- src/main/java/tgpr/tricount/controller/add_tricountController.java
+++ src/main/java/tgpr/tricount/controller/add_tricountController.java
@@ -1,6 +1,54 @@
 package tgpr.tricount.controller;
 
-public class add_tricountController {
+import tgpr.tricount.model.Tricount;
 
-    //code controller du add_tricount
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class add_tricountController extends Tricount {
+
+    // creer les liens entre les infos entree dans le add_tricountView et la bases de données
+    // ajouter le tricount dans la BD
+
+
 }
+
+
+
+//    private String title;
+//    private String description;
+//
+//
+//    //code controller du add_tricount
+//
+//    public add_tricountController(String Title,String Description){
+//        setTitle(title);
+//        setDescription(description);
+//
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        description = description;
+//    }
+
+
+//    public Tricount addTricount() {
+//        var controller = new addtricountController();
+//        navigateTo(controller);
+//        return controller.getTricount();
+//    }
+
--- src/main/java/tgpr/tricount/view/add_tricountView.java
+++ src/main/java/tgpr/tricount/view/add_tricountView.java
@@ -1,26 +1,70 @@
 package tgpr.tricount.view;
 
 import com.googlecode.lanterna.gui2.Button;
+import tgpr.tricount.model.Tricount;
 
-public class add_tricountView {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class add_tricountView extends Tricount {
+
+
+        public static void main(String[] args) {
+            JFrame fenetre = new JFrame("Saisie de Titre et Description");
+            fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            fenetre.setSize(400, 200);
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new GridLayout(2, 2));
+
+            JLabel labelTitre = new JLabel("Titre:");
+            JTextField champTitre = new JTextField(20);
+            JLabel labelDescription = new JLabel("Description:");
+            JTextField champDescription = new JTextField(20);
+
+            panel.add(labelTitre);
+            panel.add(champTitre);
+            panel.add(labelDescription);
+            panel.add(champDescription);
+
+            JButton boutonCreate = new JButton("Create");
+            boutonCreate.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String titre = champTitre.getText();
+                    String description = champDescription.getText();
+                    JOptionPane.showMessageDialog(fenetre, "Titre : " + titre + "\nDescription : " + description);
+                }
+            });
+
+            fenetre.add(panel, BorderLayout.CENTER);
+            fenetre.add(boutonCreate, BorderLayout.SOUTH);
+
+            fenetre.setVisible(true);
+        }
+    }
+
+
 
     //code visuel du addtricount
 
+//
+//            new EmptySpace().addTo(root);
+//
+//    // crée un bouton pour l'ajout d'un membre et lui associe une fonction lambda qui sera appelée
+//    // quand on clique sur le bouton
+//    var btnAddTricount = new Button("Add Tricount", () -> {
+//        System.out.println("Add Tricount");
+//    }).addTo(root);
+//
+//
+//    var btnAddTricount = new Button("Add Tricount", () -> {
+//        System.out.println("Add Tricount");
+//        Tricount t = controller.addTricount();
+//        if (t != null)
+//            reloadData();
+//    }).addTo(root);
 
-            new EmptySpace().addTo(root);
 
-    // crée un bouton pour l'ajout d'un membre et lui associe une fonction lambda qui sera appelée
-    // quand on clique sur le bouton
-    var btnAddTricount = new Button("Add Tricount", () -> {
-        System.out.println("Add Tricount");
-    }).addTo(root);
-
-
-    var btnAddTricount = new Button("Add Tricount", () -> {
-        System.out.println("Add Tricount");
-        Tricount t = controller.addTricount();
-        if (t != null)
-            reloadData();
-    }).addTo(root);
-
-}
