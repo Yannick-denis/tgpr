44e8b696953ad34d9779cedaa2227101be5b5fd3
Axel <1904deboaxe@student.epfc.eu>
2023-11-09 15:37:29 +0100

    feat(changepassword): creation controller et view

---
+++ src/main/java/tgpr/tricount/view/ChangePasswordView.java
@@ -0,0 +1,9 @@
+package tgpr.tricount.view;
+
+import com.googlecode.lanterna.gui2.dialogs.DialogWindow;
+
+public class ChangePasswordView extends DialogWindow {
+    protected ChangePasswordView(String title) {
+        super("Change Password");
+    }
+}
