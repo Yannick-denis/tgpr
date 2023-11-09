5f78f83ba6a9eefbe68ff2bb31c6065ecc1be7e4
Axel <1904deboaxe@student.epfc.eu>
2023-11-09 16:26:48 +0100

    feat(changepassword): panel et debut errorlist

--- src/main/java/tgpr/tricount/controller/ChangePasswordController.java
+++ src/main/java/tgpr/tricount/controller/ChangePasswordController.java
@@ -1,4 +1,38 @@
 package tgpr.tricount.controller;
 
-public class ChangePasswordController {
+import com.googlecode.lanterna.gui2.Window;
+import tgpr.framework.Controller;
+import tgpr.framework.Tools;
+import tgpr.tricount.model.User;
+import tgpr.tricount.view.ChangePasswordView;
+import tgpr.framework.ErrorList;
+
+public class ChangePasswordController extends Controller {
+    private final ChangePasswordView view = new ChangePasswordView(this);
+
+    @Override
+    public Window getView() {
+        return new ChangePasswordView(this);
+    }
+    public void save(String newpassword,String confirmPassword) {
+        var error=validate(newpassword,confirmPassword);
+        if (error.isEmpty()){
+            String hashedPassword= newpassword.isBlank()? newpassword: Tools.hash(newpassword);
+//            User user =new User(mail,hashedPassword,name, User.Role.User,iban); // a modifier pour qu'il change l'user actuel au lieu d'en créer un
+//            user.save();
+            //  Controller.navigateTo();
+            view.close();
+        }else {
+            showErrors(error);
+        }
+        public ErrorList validate(String newpassword, String confirmPassword) {
+            var eror = new ErrorList();
+            if (!newpassword.equals(confirmPassword)){
+                eror.add("password must be the same", User.Fields.ConfirmPassword);
+            }
+            if (newpassword== null ||newpassword.isEmpty()){
+                eror.add(" new password required ",User.Fields.Password);
+            }
+        }
+    }
 }
--- src/main/java/tgpr/tricount/view/ChangePasswordView.java
+++ src/main/java/tgpr/tricount/view/ChangePasswordView.java
@@ -1,9 +1,44 @@
 package tgpr.tricount.view;
 
+import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.DialogWindow;
+import tgpr.framework.Layouts;
+import tgpr.tricount.controller.BalanceController;
+import tgpr.tricount.controller.ChangePasswordController;
+import tgpr.tricount.controller.TestController;
+
+import java.util.List;
 
 public class ChangePasswordView extends DialogWindow {
-    protected ChangePasswordView(String title) {
+    private final ChangePasswordController controller;
+    private TextBox oldpassword;
+    private TextBox newpassword;
+    private TextBox confirmPassword;
+    private  Button btnSave;
+
+    public ChangePasswordView(ChangePasswordController controller) {
         super("Change Password");
+        setHints(List.of(Hint.CENTERED));
+        this.controller=controller;
+        Panel root = new Panel();
+        setComponent(root);
+        Panel panel = new Panel().setLayoutManager(new GridLayout(2).setTopMarginSize(1).setVerticalSpacing(0))
+                .setLayoutData(Layouts.LINEAR_CENTER).addTo(root);
+    }
+
+    private Panel panelButon() {
+        Panel panel = new Panel().setLayoutManager(new GridLayout(2).setTopMarginSize(1).setVerticalSpacing(1))
+                .setLayoutData(Layouts.LINEAR_CENTER);
+        btnSave = new Button("SAVE",()->{
+            save();
+        }).setEnabled(false).addTo(panel);
+        Button btnClose = new Button("close",()->{
+            close();
+
+        }).addTo(panel);
+        return panel;
+    }
+    private void save() {
+        controller.save(newpassword.getText(),comfirmPassword.getText());
     }
 }
