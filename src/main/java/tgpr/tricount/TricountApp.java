3fee6e3ede58ed5bb84f652f4990803c2bba72f7
alison honai <1208alhonai@student.epfc.eu>
2023-11-10 23:22:24 +0100

    refarctor(edit_template): modification du code

--- src/main/java/tgpr/tricount/TricountApp.java
+++ src/main/java/tgpr/tricount/TricountApp.java
@@ -3,10 +3,8 @@ package tgpr.tricount;
 import tgpr.framework.Controller;
 import tgpr.framework.Model;
 import tgpr.tricount.controller.AddTemplateController;
-import tgpr.tricount.controller.TestController;
-import tgpr.tricount.view.AddTemplateView;
-
-import java.awt.*;
+import tgpr.tricount.model.Template;
+import tgpr.tricount.model.Tricount;
 
 public class TricountApp {
     public final static String DATABASE_SCRIPT_FILE = "/database/tgpr-2324-c01.sql";
@@ -15,7 +13,7 @@ public class TricountApp {
         if (!Model.checkDb(DATABASE_SCRIPT_FILE))
             Controller.abort("Database is not available!");
         else {
-            Controller.navigateTo(new AddTemplateController());
+            Controller.navigateTo(new AddTemplateController(Template.getByKey(0), Tricount.getByKey(4)));
         }
     }
 }
--- src/main/java/tgpr/tricount/controller/AddTemplateController.java
+++ src/main/java/tgpr/tricount/controller/AddTemplateController.java
@@ -1,3 +1,5 @@
+
+// AddTemplateController.java
 package tgpr.tricount.controller;
 
 import com.googlecode.lanterna.gui2.Window;
@@ -7,28 +9,38 @@ import tgpr.tricount.model.Tricount;
 import tgpr.tricount.view.AddTemplateView;
 
 public class AddTemplateController extends Controller {
-    private AddTemplateView addTemplateView= new AddTemplateView(this, new Template("Benoit ne paye rien", 2));
+    private final AddTemplateView addTemplateView;
     private Template template;
-    public AddTemplateController() {
+
+    private Tricount tricount;
+
+    public AddTemplateController(Template template, Tricount tricount) {
+
+        this.template = template;
+        this.tricount = tricount;
+        addTemplateView = new AddTemplateView(this);
     }
 
-
-    public void add(String title) {
-        Template template1 =new Template(title, 0);
-        template.save().setTitle(title);
+    public Template getTemplate() {
+        return template;
     }
 
-    public void update(String title, int id){
-        Template exist = Template.getByKey(id);
-        if (exist != null){
-            exist.setTitle(title);
-            exist.save().setTitle(title);
-
+    public void onSave() {
+        if(template == null) {
+          template = new Template("", tricount.getId());
         }
-
+        String enteredTitle = addTemplateView.getTxtTitle().getText();
+        template.setTitle(enteredTitle);
+        template.save();
+        addTemplateView.close();
     }
+
+    public void onCancel() {
+        addTemplateView.close();
+    }
+
     @Override
     public Window getView() {
-        return new AddTemplateView(this, new Template("Benoit ne paye rien", 2 ));
+        return addTemplateView;
     }
 }
