2838d7e5adc5a45923317daea53f8a30c631f25e
Yannick Denis <1509yadenis@student.epfc.eu>
2023-11-15 09:26:56 +0100

    refactor(all):supression des comentaire inutil

--- src/main/java/tgpr/tricount/view/TricountListView.java
+++ src/main/java/tgpr/tricount/view/TricountListView.java
@@ -34,11 +34,11 @@ public class TricountListView extends BasicWindow {
     private final Panel pnlProfile = new Panel();
     private final TextBox filter;
     private final Paginator pagination;
-    private  Menu menuFile = new Menu("File");
+    private Menu menuFile = new Menu("File");
     private final Button createTricount;
-    private    Button close;
-//    private final profileController controllerProfil;
-    public TricountListView(TricountListController controller , profileController controllerProfil) {
+    private Button close;
+
+    public TricountListView(TricountListController controller, profileController controllerProfil) {
         this.controller = controller;
         setTitle(getTitleWithUser());
         setHints(List.of(Hint.EXPANDED));
@@ -53,133 +53,95 @@ public class TricountListView extends BasicWindow {
         menuFile.add(menuLogout);
 
 
+        pnlEnTete = Panel.gridPanel(3, Spacing.of(0));
 
-//        createMenu(controllerProfil).sizeTo(2).addTo(root);
-//        root.setFillColorOverride(TextColor.ANSI.RED);
-////        pnlBody.setFillColorOverride(TextColor.ANSI.RED);
-//        root.addComponent(menuFile , BorderLayout.Location.TOP);
-
-
-
-//        pnlEnTete = Panel.gridPanel(2, Spacing.of(0) );
-//        pnlEnTete = new Panel().setLayoutManager(new GridLayout(2).setTopMarginSize(0).setVerticalSpacing(0))
-//                .setLayoutData(Layouts.LINEAR_BEGIN);
-        pnlEnTete = Panel.gridPanel(3, Spacing.of(0) );
-//        pnlEnTete.setFillColorOverride(TextColor.ANSI.RED);
         pnlFile = Panel.gridPanel(2);
-//        pnlFile.setFillColorOverride(TextColor.ANSI.RED);
-
         pnlFile.addComponent(createMenu(controllerProfil));
-        pnlEnTete.addComponent(pnlFile,BorderLayout.Location.TOP);
+        pnlEnTete.addComponent(pnlFile, BorderLayout.Location.TOP);
         root.addComponent(pnlEnTete);
         root.addComponent(pnlEnTete, BorderLayout.Location.TOP);
-
-//        createMenu(controllerProfil).addTo(root);
-//        MenuBar menuBar = new MenuBar().addTo(pnlProfile);
-//        pnlEnTete.addComponent(pnlProfile);
-//        menuFile = new Menu("File");
-//        menuBar.add(menuFile);
-////        MenuItem menuprofile = new MenuItem("View Profile" ,  () -> {Controller.navigateTo(controllerProfil);});
-//        menuFile.add(menuprofile);
         new EmptySpace().addTo(pnlEnTete);
         new EmptySpace().addTo(pnlEnTete);
-//        pnlProfile = Panel.verticalPanel();
-
 
 
         pnlEnTete.addComponent(new Label("filter:"));
         filter = new TextBox().addTo(pnlEnTete).sizeTo(30).setTextChangeListener((txt, filter) -> reloadData());
-//        filter.addTo(pnlEnTete);
-        pnlEnTete.addComponent(filter , BorderLayout.Location.BOTTOM);
-        pnlBody = Panel.gridPanel(3, Spacing.of(0) );
-//        root.addComponent(pnlBody);
+        pnlEnTete.addComponent(filter, BorderLayout.Location.BOTTOM);
+        pnlBody = Panel.gridPanel(3, Spacing.of(0));
         root.addComponent(pnlBody, BorderLayout.Location.CENTER);
 
         pnlBasDePage = new Panel().setLayoutManager(new BorderLayout());
         root.addComponent(pnlBasDePage, BorderLayout.Location.BOTTOM);
-        createTricount = new Button("Create a new Tricount" , () -> Controller.navigateTo(new AddTricountControler(Security.getLoggedUser())));
+        createTricount = new Button("Create a new Tricount", () -> Controller.navigateTo(new AddTricountControler(Security.getLoggedUser())));
         pnlBasDePage.addComponent(createTricount, BorderLayout.Location.LEFT);
-        pagination = new Paginator(this,12,this::pageChanged);
+        pagination = new Paginator(this, 12, this::pageChanged);
         pnlBasDePage.addComponent(pagination, BorderLayout.Location.RIGHT);
         reloadData();
-        close=new Button("ce bouton naparait pas",()->close());
+        close = new Button("ce bouton naparait pas", () -> System.exit(0));
         close.setVisible(false);
         close.addTo(root);
-        addShortcut(close,KeyStroke.fromString("<A-c>"));
-
-
+        addShortcut(close, KeyStroke.fromString("<A-c>"));
 
 
     }
 
-        public void pageChanged ( int page){
-            // Remarque: les pages sont indexées à partir de zéro
-            System.out.println("L'utilisateur a demandé la page " + (page + 1));
-            // aller en db pour récupérer les tricounts de la page courante (limit)
-            // appeler reloadData() pour afficher
-            reloadData();
-        }
-        public void reloadData () {
-            var tricounts = controller.getTricounts(filter.getText());
-            pagination.setCount(tricounts.size());
-            pnlBody.removeAllComponents();
-            int start = pagination.getStart();
-            int end = Math.min(start + 12, tricounts.size());
-            for (int i = start; i < end; ++i) {
-                var tricount = tricounts.get(i);
-                Panel p = Panel.verticalPanel();
-                new Label(tricount.getTitle()).setForegroundColor(TextColor.ANSI.BLUE).center().addTo(p);
-                new Label(tricount.getDescription() == null ? "No description" : tricount.getDescription()).setForegroundColor(TextColor.ANSI.BLACK_BRIGHT).center().addTo(p);
-                new Label("Created By " + tricount.getCreator().getFullName()).center().addTo(p);
-                int nbrParticipant = tricount.getParticipants().size();
-                if ((nbrParticipant - 1) == 0) {
-                    new Label("with no friends ").center().addTo(p);
-                } else {
-                    new Label("with " + (nbrParticipant - 1) + "friends ").center().addTo(p);
-                }
-                new Button("Open" , () -> Controller.navigateTo(new ViewTricoutController(tricount,Security.getLoggedUser()))).center().addTo(p);
-                p.sizeTo(35, 5);
-                pnlBody.addComponent(p.withBorder(Borders.singleLine()));
-                }
-
-
-        }
-        private String getTitleWithUser() {
-            return "Tricount (" + Security.getLoggedUser().getMail() + " - User )";
-        }
-        public void profile(){
-            Panel pnlProfile = Panel.verticalPanel();
-            pnlProfile.withBorder(Borders.singleLine("View"));
-            new Label("Hey Boris!").addTo(pnlProfile);
-            setHints(List.of(Hint.CENTERED));
-            setCloseWindowWithEscape(true);
-            pnlBody.addComponent(pnlProfile.withBorder(Borders.singleLine("View Profile")));
-        }
-        private MenuBar createMenu(profileController controllerProfil){
-            MenuBar menuBar = new MenuBar();
-            menuBar.add(menuFile);
-            MenuItem menuprofile = new MenuItem("View Profile" ,  () -> {Controller.navigateTo(controllerProfil);});
-            menuFile.add(menuprofile);
-            MenuItem menuLogout = new MenuItem("Logout", controller::logout);
-            addShortcut(menuLogout, KeyStroke.fromString("<A-c>"));
-            menuFile.add(menuLogout);
-            return menuBar;
-
-//            MenuBar menuBar = new MenuBar().addTo(root);
-//            menuFile = new Menu("File");
-//            menuBar.add(menuFile);
-//            MenuItem menuLogout = new MenuItem("Logout", controller::logout);
-//            menuFile.add(menuLogout);
-
-        }
-//    private MenuBar createMenu() {
-//        MenuBar menuBar = new MenuBar();
-//        menuBar.add(menuFile);
-//        addShortcut(menuFile, KeyStroke.fromString("<A-f>"));
-//        MenuItem menuLogout = new MenuItem("Logout", controller::logout);
-//        menuFile.add(menuLogout);
-//        MenuItem menuExit = new MenuItem("Exit", controller::exit);
-//        menuFile.add(menuExit);
-//        return menuBar;
-//    }
+    public void pageChanged(int page) {
+        // Remarque: les pages sont indexées à partir de zéro
+        System.out.println("L'utilisateur a demandé la page " + (page + 1));
+        // aller en db pour récupérer les tricounts de la page courante (limit)
+        // appeler reloadData() pour afficher
+        reloadData();
     }
+
+    public void reloadData() {
+        var tricounts = controller.getTricounts(filter.getText());
+        pagination.setCount(tricounts.size());
+        pnlBody.removeAllComponents();
+        int start = pagination.getStart();
+        int end = Math.min(start + 12, tricounts.size());
+        for (int i = start; i < end; ++i) {
+            var tricount = tricounts.get(i);
+            Panel p = Panel.verticalPanel();
+            new Label(tricount.getTitle()).setForegroundColor(TextColor.ANSI.BLUE).center().addTo(p);
+            new Label(tricount.getDescription() == null ? "No description" : tricount.getDescription()).setForegroundColor(TextColor.ANSI.BLACK_BRIGHT).center().addTo(p);
+            new Label("Created By " + tricount.getCreator().getFullName()).center().addTo(p);
+            int nbrParticipant = tricount.getParticipants().size();
+            if ((nbrParticipant - 1) == 0) {
+                new Label("with no friends ").center().addTo(p);
+            } else {
+                new Label("with " + (nbrParticipant - 1) + "friends ").center().addTo(p);
+            }
+            new Button("Open", () -> Controller.navigateTo(new ViewTricoutController(tricount, Security.getLoggedUser()))).center().addTo(p);
+            p.sizeTo(35, 5);
+            pnlBody.addComponent(p.withBorder(Borders.singleLine()));
+        }
+
+
+    }
+
+    private String getTitleWithUser() {
+        return "Tricount (" + Security.getLoggedUser().getMail() + " - User )";
+    }
+
+    public void profile() {
+        Panel pnlProfile = Panel.verticalPanel();
+        pnlProfile.withBorder(Borders.singleLine("View"));
+        new Label("Hey Boris!").addTo(pnlProfile);
+        setHints(List.of(Hint.CENTERED));
+        setCloseWindowWithEscape(true);
+        pnlBody.addComponent(pnlProfile.withBorder(Borders.singleLine("View Profile")));
+    }
+
+    private MenuBar createMenu(profileController controllerProfil) {
+        MenuBar menuBar = new MenuBar();
+        menuBar.add(menuFile);
+        MenuItem menuprofile = new MenuItem("View Profile", () -> {
+            Controller.navigateTo(controllerProfil);
+        });
+        menuFile.add(menuprofile);
+        MenuItem menuLogout = new MenuItem("Logout", controller::logout);
+        addShortcut(menuLogout, KeyStroke.fromString("<A-c>"));
+        menuFile.add(menuLogout);
+        return menuBar;
+    }
+}
